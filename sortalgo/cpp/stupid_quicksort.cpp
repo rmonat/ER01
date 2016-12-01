69de4286f29ab5d5d14f8da0e4752a9bfa550aa8
chardetm <chardetm@gmail.com>
2016-12-01 12:00:24 +0100

    Timing C++ (affichage du temps en ms sur stdout)

--- sortalgo/cpp/stupid_quicksort.cpp
+++ sortalgo/cpp/stupid_quicksort.cpp
@@ -2,23 +2,35 @@
 #include <iostream>
 #include <vector>
 #include <cstdint>
+#include <cmath>
+#include <chrono>
 
 using namespace std;
 
 int n;
 vector<int_fast32_t> tab;
 
+template <class TimeT>
+unsigned long time_ms (TimeT before, TimeT after) {
+	auto diff = after-before;
+	
+	auto doubledur = chrono::duration <double, milli> (diff).count();
+	auto uldur = static_cast<unsigned long>(round(doubledur));
+	
+	return uldur;
+}
+
 
 void quicksort(int begin, int end)
 {
-	if(begin < end)
+	if (begin < end)
 	{
 		int i = begin;
 		int pivot = tab[begin];
 
-		for(int j = begin+1; j <= end; j++)
+		for (int j = begin+1; j <= end; j++)
 		{
-		    if(tab[j] <= pivot)
+		    if (tab[j] <= pivot)
 		    {
 				i++;
 				swap(tab[i], tab[j]);
@@ -37,8 +49,10 @@ int main()
 	tab.resize(n);
 	for(int i = 0; i < n; i++)
 		cin >> tab[i];
+	
+	auto before = chrono::high_resolution_clock::now();
 	quicksort(0, tab.size()-1);
-	for(int i = 0; i < n; i++)
-		cout << tab[i] << " ";
-	cout << endl;
+	auto after = chrono::high_resolution_clock::now();
+	
+	cout << time_ms(before, after) << endl;
 }
